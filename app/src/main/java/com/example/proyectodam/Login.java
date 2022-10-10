a6dab7dbfbfd6c83b03df968d7b60dcd3b93baef
resua <resuavidal@gmail.com>
2022-10-11 00:27:54 +0200

    Some more view Improves

--- app/src/main/java/com/example/proyectodam/Login.java
+++ app/src/main/java/com/example/proyectodam/Login.java
@@ -3,6 +3,9 @@ package com.example.proyectodam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Control definitions
+        EditText txtUsername = (EditText) findViewById(R.id.inputName);
+        EditText txtPassword = (EditText) findViewById(R.id.inputPassword);
+        EditText txtPassword2 = (EditText) findViewById(R.id.inputPassword2);
+        EditText txtEmail = (EditText) findViewById(R.id.inputEmail);
+
+
+
     }
 }
