83cd37044bf60d960ebd9b8a4ec0c8557dd119fb
resua <resuavidal@gmail.com>
2022-10-10 23:11:07 +0200

    Added top bar header (Not Finished) Some view Improves

--- app/src/main/java/com/example/proyectodam/mainMenu.java
+++ app/src/main/java/com/example/proyectodam/mainMenu.java
@@ -1,8 +1,12 @@
 package com.example.proyectodam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class mainMenu extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class mainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.header1, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.headerIconOption1:
+                Toast.makeText(mainMenu.this, "Opcion 1 pulsado", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
